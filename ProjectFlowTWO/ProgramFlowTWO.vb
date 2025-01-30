e806a0fe5d09c9fce22a39c24f7a6b85aeb9c93d
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-30 08:28:46 -0700

    Good Do loop example

---
+++ ProjectFlowTWO/ProgramFlowTWO.vb
@@ -0,0 +1,51 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module ProgramFlowTWO
+
+    Sub Main()
+        Dim num1 As Integer
+
+        Do
+
+            Console.WriteLine("In the first loop. Loop While num1 <= 3")
+            num1 += 1
+            Console.WriteLine($"First number is {num1}.")
+            Console.WriteLine()
+
+        Loop While num1 <= 3
+
+        Console.WriteLine()
+
+        Do
+
+            Console.WriteLine("In the second loop. Loop While num1 <=")
+            num1 += 1
+            Console.WriteLine($"First number is {num1}.")
+            Console.WriteLine()
+
+        Loop While num1 <= 3
+
+        Console.WriteLine()
+
+        Do Until num1 >= 3
+
+            Console.WriteLine("In the third loop. Loop Until num1 >=")
+            num1 += 1
+            Console.WriteLine($"First number is {num1}.")
+            Console.WriteLine()
+        Loop
+
+        Do While num1 >= 3
+
+            Console.WriteLine("In the fourth loop. Loop Until num1 >=")
+            num1 -= 1
+            Console.WriteLine($"First number is {num1}.")
+            Console.WriteLine()
+        Loop
+
+        Console.WriteLine()
+    End Sub
+
+End Module
