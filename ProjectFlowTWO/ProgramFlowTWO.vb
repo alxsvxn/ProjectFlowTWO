344b0bb4d2f80787f7b59621f997f5003a8c2691
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-31 08:59:40 -0700

    Adjust Do loops, Added extra loop for second user input

--- ProjectFlowTWO/ProgramFlowTWO.vb
+++ ProjectFlowTWO/ProgramFlowTWO.vb
@@ -3,6 +3,7 @@ Option Strict On
 Option Compare Text
 Imports System.Diagnostics.Contracts
 Imports System.IO
+Imports System.Linq.Expressions
 
 Module ProgramFlowTWO
 
@@ -10,22 +11,55 @@ Module ProgramFlowTWO
         Dim num1 As Integer
         Dim getOutOfHere As Boolean = False
         Dim userInput As String
-
+        Dim userInput2 As String
+        Dim num2 As Integer
         Do
-            Console.WriteLine("WELCOME, please enter a number.")
-            Console.WriteLine("Or enter Q to quit.")
-            userInput = Console.ReadLine()
-            Console.WriteLine($"You entered {userInput}.")
+            Do
+                Console.WriteLine("WELCOME, please enter a number.")
+                Console.WriteLine("Or enter Q to quit.")
+                userInput = Console.ReadLine()
+                Console.WriteLine($"You entered {userInput}.")
 
-            Try
-                num1 = CInt(userInput)
-                Console.WriteLine($"{userInput} is a great number!")
-                Console.WriteLine()
-            Catch ex As Exception
-                Console.WriteLine($"{userInput} is not a number... :(")
-                Console.WriteLine()
-            End Try
+                Try
+                    num1 = CInt(userInput)
+                    getOutOfHere = True
 
+                    Console.WriteLine($"{userInput} is a great number!")
+                    Console.WriteLine()
+
+                Catch ex As Exception
+
+                    If userInput <> "Q" Then
+                        Console.WriteLine($"{userInput} is not a number :/")
+                    Else
+                        getOutOfHere = True
+                    End If
+                End Try
+
+            Loop Until getOutOfHere = True
+
+            getOutOfHere = False
+
+            Do While userInput <> "Q" And getOutOfHere = False
+
+                Console.WriteLine("Please enter another number.")
+                Console.WriteLine("Or enter Q to quit.")
+                userInput2 = Console.ReadLine()
+                Console.WriteLine($"You entered {userInput2}.")
+
+                Try
+                    num2 = CInt(userInput2)
+                    getOutOfHere = True
+                    Console.WriteLine($"{userInput2} is a great number!")
+                    Console.WriteLine()
+                Catch ex As Exception
+                    If userInput2 <> "Q" Then
+                        Console.WriteLine($"{userInput2} is not a number :/")
+                    Else
+                        getOutOfHere = True
+                    End If
+                End Try
+            Loop
 
         Loop While userInput <> "Q"
 
